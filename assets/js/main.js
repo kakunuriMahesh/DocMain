b64a2907760f418731d3cdad8c9c45647dea19d1
Mahesh Kakunuri <maheshkakunuri9@gmail.com>
2024-09-25 10:42:46 +0530

    artical__arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,6 +93,29 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// const sections = document.querySelectorAll("section[id]");
+
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
+
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight,
+//       sectionTop = current.offsetTop - 58,
+//       sectionId = current.getAttribute("id");
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.add("active-link");
+//     } else {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.remove("active-link");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", scrollActive);
+
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
@@ -105,16 +128,17 @@ function scrollActive() {
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .querySelector(".nav__list a[href*=" + sectionId + "]")
         .classList.add("active-link");
     } else {
       document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .querySelector(".nav__list a[href*=" + sectionId + "]")
         .classList.remove("active-link");
     }
   });
 }
 window.addEventListener("scroll", scrollActive);
+
 
 /*=============== LIGHT DARK THEME ===============*/
 const themeButton = document.getElementById("theme-button");
